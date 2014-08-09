09d859d9febc524a03ac06adcc63c039798a9e1c
sbmaxx <sbmaxx@gmail.com>
2014-08-09 12:18:11 +0400

    changed domain for jquery

--- megafonBalanceDetailsBookmarklet.js
+++ megafonBalanceDetailsBookmarklet.js
@@ -1,13 +1,18 @@
 var head = document.querySelector('head');
+
 var jq = document.createElement('script');
-var js = document.createElement('script');
-var css = document.createElement('link');
 jq.type = 'text/javascript';
-js.src = 'http://yandex.st/jquery/2.1.1/jquery.min.js';
+jq.src = 'http://yastatic.net/jquery/2.1.1/jquery.min.js';
+
+var js = document.createElement('script');
 js.type = 'text/javascript';
 js.src = 'file:///Users/sbmaxx/Development/helpers/megafonBalanceDetails.js';
+
+var css = document.createElement('link');
 css.href = 'file:///Users/sbmaxx/Development/helpers/megafonBalanceDetails.css';
 css.rel = 'stylesheet';
 css.type = 'text/css';
+
+head.appendChild(jq);
 head.appendChild(js);
 head.appendChild(css);
